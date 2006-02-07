92187a5530e2c43d8e005e9e56abc4dd499e2b1e
burnett <>
2006-02-07 04:30:20 +0000

    tagging after Gaudi fix

--- src/Dll/AcdRecon_load.cxx
+++ src/Dll/AcdRecon_load.cxx
@@ -3,7 +3,7 @@
 * @brief This is needed for forcing the linker to load all components
 * of the library.
 *
-*  $Header: /nfs/slac/g/glast/ground/cvs/AcdRecon/src/Dll/AcdRecon_load.cxx,v 1.3 2004/08/24 21:10:48 hansl Exp $
+*  $Header: /nfs/slac/g/glast/ground/cvs/AcdRecon/src/Dll/AcdRecon_load.cxx,v 1.4 2005/11/10 15:40:55 burnett Exp $
 */
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
@@ -12,4 +12,6 @@ DECLARE_FACTORY_ENTRIES( AcdRecon ) {
     DECLARE_ALGORITHM( AcdReconAlg );
     DECLARE_ALGORITHM( AcdDisplay );
     DECLARE_TOOL ( AcdTkrIntersectTool );
+    DECLARE_TOOL ( AcdPha2MipTool );
+    DECLARE_TOOL ( AcdPocaTool );
 } 
