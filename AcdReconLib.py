f9c3bbd766eb0d1f293d7d35e0d4117e13865dc4
jrb <>
2009-12-21 20:09:43 +0000

    Add dependence on rootLibs

--- AcdReconLib.py
+++ AcdReconLib.py
@@ -1,4 +1,4 @@
-#$Header$
+#$Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/AcdRecon/AcdReconLib.py,v 1.1 2008/07/09 21:13:39 glastrm Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['AcdRecon'])
@@ -6,6 +6,7 @@ def generate(env, **kw):
     env.Tool('guiLib')
     env.Tool('identsLib')
     env.Tool('AcdUtilLib')
+    env.Tool('addLibrary', library = env['rootLibs'])
 
 def exists(env):
     return 1;
