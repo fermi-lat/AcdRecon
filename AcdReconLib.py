14e97f366f684a4ca8f231b6b717b4779bda1a6f
jrb <>
2012-08-18 00:14:44 +0000

    support no-install headers on Windows

--- AcdReconLib.py
+++ AcdReconLib.py
@@ -1,12 +1,18 @@
-#$Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/AcdRecon/AcdReconLib.py,v 1.1 2008/07/09 21:13:39 glastrm Exp $
+#$Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/AcdRecon/AcdReconLib.py,v 1.2 2009/12/21 20:09:43 jrb Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['AcdRecon'])
+        if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+	    env.Tool('findPkgPath', package = 'AcdRecon') 
     env.Tool('EventLib')
     env.Tool('guiLib')
     env.Tool('identsLib')
     env.Tool('AcdUtilLib')
     env.Tool('addLibrary', library = env['rootLibs'])
+    if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+        env.Tool('findPkgPath', package='GuiSvc')
+        env.Tool('findPkgPath', package='TkrUtil')
+        env.Tool('findPkgPath', package='LdfEvent')
 
 def exists(env):
     return 1;
